9727e2ff460f074bdcf2daec633f531720633146
jaivatsan-elango-sundaram <je.sundaram@gmail.com>
2025-02-16 15:42:07 -0500

    added code for other types + fixed naming convention

--- BurgsDevelopment/combining_dataframes.py
+++ BurgsDevelopment/combining_dataframes.py
@@ -1,50 +1,78 @@
 import pandas
 #hallways
-hallway1 = pandas.read_csv("../data/hallway1.csv")
-hallway1 = hallway1.drop(0)
-hallway2 = pandas.read_csv("../data/hallway2.csv")
-hallway2 = hallway2.drop(0)
-hallway3 = pandas.read_csv("../data/hallway3.csv")
-hallway3 = hallway3.drop(0)
-all_hallways = pandas.concat([hallway1, hallway2], axis = 0)
-all_hallways = pandas.concat([all_hallways,hallway3], axis = 0)
+hallway_1 = pandas.read_csv("../data/hallway1.csv")
+hallway_1 = hallway_1.drop(0)
+hallway_2 = pandas.read_csv("../data/hallway2.csv")
+hallway_2 = hallway_2.drop(0)
+hallway_3 = pandas.read_csv("../data/hallway3.csv")
+hallway_3 = hallway_3.drop(0)
+all_hallways = pandas.concat([hallway_1, hallway_2], axis = 0)
+all_hallways = pandas.concat([all_hallways,hallway_3], axis = 0)
 all_hallways = all_hallways.drop(all_hallways.columns[1], axis = 1)
-print(all_hallways)
+#print(all_hallways)
 
 #chairs
+open_chair_1 = pandas.read_csv("../data/openchair1.csv")
+open_chair_1 = open_chair_1.drop(0)
+open_chair_2 = pandas.read_csv("../data/openchair2.csv")
+open_chair_2 = open_chair_2.drop(0)
+open_chair_3 = pandas.read_csv("../data/openchair3.csv")
+open_chair_3 = open_chair_3.drop(0)
 
-openchair1 = pandas.read_csv("../data/openchair1.csv")
-openchair1 = openchair1.drop(0)
-openchair2 = pandas.read_csv("../data/openchair2.csv")
-openchair2 = openchair2.drop(0)
-openchair3 = pandas.read_csv("../data/openchair3.csv")
-openchair3 = openchair3.drop(0)
-
-all_open_chairs = pandas.concat([openchair1, openchair2], axis = 0)
-all_open_chairs = pandas.concat([all_open_chairs, openchair3], axis = 0)
+all_open_chairs = pandas.concat([open_chair_1, open_chair_2], axis = 0)
+all_open_chairs = pandas.concat([all_open_chairs, open_chair_3], axis = 0)
 all_open_chairs = all_open_chairs.drop(all_open_chairs.columns[1], axis = 1)
 
+#print(all_open_chairs)
 
 #blinds
-mrblinds1 = pandas.read_csv("../data/mrblinds1.csv")
-mrblinds1 = mrblinds1.drop(0)
-mrblinds2 = pandas.read_csv("../data/mrblinds2.csv")
-mrblinds2 = mrblinds2.drop(0)
-mrblinds3 = pandas.read_csv("../data/mrblinds3.csv")
-mrblinds3 = mrblinds3.drop(0)
+mr_blinds_1 = pandas.read_csv("../data/mrblinds1.csv")
+mr_blinds_1 = mr_blinds_1.drop(0)
+mr_blinds_2 = pandas.read_csv("../data/mrblinds2.csv")
+mr_blinds_2 = mr_blinds_2.drop(0)
+mr_blinds_3 = pandas.read_csv("../data/mrblinds3.csv")
+mr_blinds_3 = mr_blinds_3.drop(0)
+mr3_blinds_1 = pandas.read_csv("../data/mr3blinds1.csv")
+mr3_blinds_1 = mr3_blinds_1.drop(0)
+mr3_blinds_2 = pandas.read_csv("../data/mr3blinds2.csv")
+mr3_blinds_2 = mr3_blinds_2.drop(0)
+mr3_blinds_3 = pandas.read_csv("../data/mr3blinds3.csv")
+mr3_blinds_3 = mr3_blinds_3.drop(0)
 
-all_mr_blinds = pandas.concat([mrblinds1, mrblinds2], axis=0)
-all_mr_blinds = pandas.concat([all_mr_blinds, mrblinds3], axis = 0)
+all_mr_blinds = pandas.concat([mr_blinds_1, mr_blinds_2], axis=0)
+all_mr_blinds = pandas.concat([all_mr_blinds, mr_blinds_3], axis = 0)
+all_mr_blinds = pandas.concat([all_mr_blinds, mr3_blinds_1], axis = 0)
+all_mr_blinds = pandas.concat([all_mr_blinds, mr3_blinds_2], axis = 0)
+all_mr_blinds = pandas.concat([all_mr_blinds, mr3_blinds_3], axis = 0)
 all_mr_blinds = all_mr_blinds.drop(all_mr_blinds.columns[1], axis = 1)
 
-#windows
-mrwindows1 = pandas.read_csv("../data/mrwindows1.csv")
-mrwindows1 = mrwindows1.drop(0)
-mrwindows2 = pandas.read_csv("../data/mrwindows2.csv")
-mrwindows2 = mrwindows2.drop(0)
-mrwindows3 = pandas.read_csv("../data/mrwindows3.csv")
-mrwindows3 = mrwindows3.drop(0)
+#print(all_mr_blinds)
 
-all_mr_windows = pandas.concat([mrwindows1, mrwindows2], axis = 0)
-all_mr_windows = pandas.concat([all_mr_windows, mrwindows3], axis = 0)
+#windows
+mr_windows_1 = pandas.read_csv("../data/mrwindows1.csv")
+mr_windows_1 = mr_windows_1.drop(0)
+mr_windows_2 = pandas.read_csv("../data/mrwindows2.csv")
+mr_windows_2 = mr_windows_2.drop(0)
+mr_windows_3 = pandas.read_csv("../data/mrwindows3.csv")
+mr_windows_3 = mr_windows_3.drop(0)
+
+all_mr_windows = pandas.concat([mr_windows_1, mr_windows_2], axis = 0)
+all_mr_windows = pandas.concat([all_mr_windows, mr_windows_3], axis = 0)
 all_mr_windows = all_mr_windows.drop(all_mr_windows.columns[1], axis = 1)
+
+#print(all_mr_windows)
+
+#entrance
+pp_entrance = pandas.read_csv("../data/pplab.csv")
+#print(pp_entrance)
+
+#lab
+pp_lab_1 = pandas.read_csv("../data/pplab.csv")
+pp_lab_1 = pp_lab_1.drop(0)
+pp_lab_2 = pandas.read_csv("../data/pplab2.csv")
+pp_lab_2 = pp_lab_2.drop(0)
+
+all_lab = pandas.concat([pp_lab_1, pp_lab_2],  axis = 0)
+all_lab = all_lab.drop(all_lab.columns[1], axis = 1)
+
+print(all_lab)
