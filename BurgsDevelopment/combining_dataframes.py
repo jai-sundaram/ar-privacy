a04ce16c06623836e1411f9d37d16d635be15702
jaivatsan-elango-sundaram <je.sundaram@gmail.com>
2025-02-23 23:53:50 -0500

    Added categories for each dataframe

--- BurgsDevelopment/combining_dataframes.py
+++ BurgsDevelopment/combining_dataframes.py
@@ -9,6 +9,9 @@ hallway_3 = hallway_3.drop(0)
 all_hallways = pandas.concat([hallway_1, hallway_2], axis = 0)
 all_hallways = pandas.concat([all_hallways,hallway_3], axis = 0)
 all_hallways = all_hallways.drop(all_hallways.columns[1], axis = 1)
+hallways_length = len(all_hallways['time'])
+hallways_category = ["HALLWAY"] * hallways_length
+all_hallways["category"] = hallways_category
 #print(all_hallways)
 
 #chairs
@@ -18,11 +21,12 @@ open_chair_2 = pandas.read_csv("../data/openchair2.csv")
 open_chair_2 = open_chair_2.drop(0)
 open_chair_3 = pandas.read_csv("../data/openchair3.csv")
 open_chair_3 = open_chair_3.drop(0)
-
 all_open_chairs = pandas.concat([open_chair_1, open_chair_2], axis = 0)
 all_open_chairs = pandas.concat([all_open_chairs, open_chair_3], axis = 0)
 all_open_chairs = all_open_chairs.drop(all_open_chairs.columns[1], axis = 1)
-
+chairs_length = len(all_open_chairs['time'])
+chairs_category = ["OPEN_CHAIRS"] * chairs_length
+all_open_chairs["category"] = chairs_category
 #print(all_open_chairs)
 
 #blinds
@@ -38,14 +42,15 @@ mr3_blinds_2 = pandas.read_csv("../data/mr3blinds2.csv")
 mr3_blinds_2 = mr3_blinds_2.drop(0)
 mr3_blinds_3 = pandas.read_csv("../data/mr3blinds3.csv")
 mr3_blinds_3 = mr3_blinds_3.drop(0)
-
 all_mr_blinds = pandas.concat([mr_blinds_1, mr_blinds_2], axis=0)
 all_mr_blinds = pandas.concat([all_mr_blinds, mr_blinds_3], axis = 0)
 all_mr_blinds = pandas.concat([all_mr_blinds, mr3_blinds_1], axis = 0)
 all_mr_blinds = pandas.concat([all_mr_blinds, mr3_blinds_2], axis = 0)
 all_mr_blinds = pandas.concat([all_mr_blinds, mr3_blinds_3], axis = 0)
 all_mr_blinds = all_mr_blinds.drop(all_mr_blinds.columns[1], axis = 1)
-
+blinds_length = len(all_mr_blinds['time'])
+blinds_category = ["BLINDS"] * blinds_length
+all_mr_blinds["category"] = blinds_category
 #print(all_mr_blinds)
 
 #windows
@@ -55,15 +60,19 @@ mr_windows_2 = pandas.read_csv("../data/mrwindows2.csv")
 mr_windows_2 = mr_windows_2.drop(0)
 mr_windows_3 = pandas.read_csv("../data/mrwindows3.csv")
 mr_windows_3 = mr_windows_3.drop(0)
-
 all_mr_windows = pandas.concat([mr_windows_1, mr_windows_2], axis = 0)
 all_mr_windows = pandas.concat([all_mr_windows, mr_windows_3], axis = 0)
 all_mr_windows = all_mr_windows.drop(all_mr_windows.columns[1], axis = 1)
-
+windows_length = len(all_mr_windows['time'])
+windows_category = ["WINDOWS"] * windows_length
+all_mr_windows["category"] = windows_category
 #print(all_mr_windows)
 
 #entrance
 pp_entrance = pandas.read_csv("../data/pplab.csv")
+entrance_length = len(pp_entrance['time'])
+entrance_category = ["ENTRANCE"] * entrance_length
+pp_entrance["category"] = entrance_category
 #print(pp_entrance)
 
 #lab
@@ -74,5 +83,7 @@ pp_lab_2 = pp_lab_2.drop(0)
 
 all_lab = pandas.concat([pp_lab_1, pp_lab_2],  axis = 0)
 all_lab = all_lab.drop(all_lab.columns[1], axis = 1)
-
+lab_length = len(all_lab['time'])
+lab_category = ["LAB"] * lab_length
+all_lab["category"] = lab_category
 print(all_lab)
